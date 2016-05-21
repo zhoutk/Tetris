c02cf6ccdbba6b35e1abe6e3c11136bf6af60f79
zhoutk <zhoutk@189.cn>
2016-05-21 08:25:20 +0800

    auto move down.

--- index.js
+++ index.js
@@ -12,11 +12,39 @@ var tetrisNext = new Tetris(nextNum,blockCtx)
 
 const LVSCS = [0,1,3,6,10]
 var gameState = false, levels = 0, scores = 0;
+var interval, TICKVAL = 250;
 
 function gameStart(){
+    clearInterval(interval)
     gameStart = true;
     tetris.draw();
     tetrisNext.draw()
+    interval = setInterval( tick, TICKVAL );
+}
+
+function generateNext(){
+    randNum = nextNum; 
+    nextNum = Math.floor(Math.random()*7)
+    tetris = new Tetris(randNum,ctx,3)
+    if(tetris.canDrawNext()){
+        tetris.draw();
+        tetrisNext.erase();
+        tetrisNext = new Tetris(nextNum,blockCtx)
+        tetrisNext.draw()
+    }else{
+        levels = 0;
+        scores = 0;
+        tetris.draw();
+        gameStart = false;
+        clearInterval(interval)
+        alert("game is over.")
+    }
+}
+
+function tick(){
+    if(!tetris.moveNext()){
+        generateNext();
+    }
 }
 
 function keyProess(e){
@@ -34,22 +62,8 @@ function keyProess(e){
             case 40:  //down key up
             case 32:  //space key up
                 if(!tetris.moveDown()){
-                    randNum = nextNum; 
-                    nextNum = Math.floor(Math.random()*7)
-                    tetris = new Tetris(randNum,ctx,3)
-                    if(tetris.canDrawNext()){
-                        tetris.draw();
-                        tetrisNext.erase();
-                        tetrisNext = new Tetris(nextNum,blockCtx)
-                        tetrisNext.draw()
-                    }else{
-                        levels = 0;
-                        scores = 0;
-                        tetris.draw();
-                        gameStart = false;
-                        alert("game is over.")
-                    }
-            }
+                    generateNext();
+                }
             break;  
         }  
     }
