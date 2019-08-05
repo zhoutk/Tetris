eefb4fed16962b21f40411407b301f3d087622a6
zhoutk <zhoutk@189.cn>
2019-08-05 13:12:56 +0800

    update to adopt new electron version.

--- main.js
+++ main.js
@@ -10,7 +10,14 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 660})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 660,
+    transparent: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html')
